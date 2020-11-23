243f548a9ed85a69d0ede0194912a44382ad5a8b
alafranc <alafranc@student.42lyon.fr>
2020-11-23 15:11:52 +0100

    some details add

--- srcs/ft_memcmp.c
+++ srcs/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: alafranc <alafranc@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 15:17:52 by alafranc          #+#    #+#             */
-/*   Updated: 2020/11/15 15:17:53 by alafranc         ###   ########lyon.fr   */
+/*   Updated: 2020/11/23 13:44:55 by alafranc         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned char	*us1;
 	unsigned char	*us2;
 
+	if (n == 0)
+		return (0);
 	i = 0;
 	us1 = (unsigned char*)s1;
 	us2 = (unsigned char*)s2;
